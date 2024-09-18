bdff0eb041ad7bc09fcd65be186764e2c65caf57
RichardJulianMakaba07 <richard.julian.m07@gmail.com>
2024-09-18 21:00:54 +0800

    Kelas B2 2024

---
+++ Kelas B2 2024/Pertemuan-1/mencari luas trapesium_Richard Julian Makaba.py
@@ -0,0 +1,7 @@
+sisi_atas = int(input('Masukkan nilai sisi atas pada trapesium: '))
+sisi_bawah = int(input('Masukkan nilai sisi bawah pada trapesium: '))
+tinggi = int(input('Masukkan nilai tinggi pada trapesium: '))
+
+penjumlahan = sisi_atas + sisi_bawah
+luas_trapesium = 1/2 * penjumlahan * tinggi
+print(luas_trapesium)
---
+++ Kelas B2 2024/Pertemuan-2/mengonversi mata uang_Richard Julian Makaba.py
@@ -0,0 +1,13 @@
+dolar_amerika = 15000
+euro = 17000
+yen = 100
+
+rupiah = int(input('Masukkan nilai rupiah: '))
+
+usd = rupiah/dolar_amerika
+eur = rupiah/euro
+jpy = rupiah/yen
+
+print(usd)
+print(eur)
+print(jpy)
