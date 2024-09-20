0db1ba925b4a4f82a8e16932d40354c1778e10a3
Richard J.M. <richard.julian.m07@gmail.com>
2024-09-20 15:19:00 +0800

    Delete Kelas B2 2024/Pertemuan-3 directory

--- Kelas B2 2024/Pertemuan-3/diskon buku.py
+++
@@ -1,8 +0,0 @@
-jumlah_buku = int(input('Masukkan jumlah buku yang dibeli: '))
-total_harga_pembelian = int(input('Masukkan total harga pembelian: '))
-
-if jumlah_buku >= 5 and total_harga_pembelian > 100000:
-    harga_akhir = total_harga_pembelian * 20 / 100
-    print(f'Selamat Anda mendapat diskon sebesar 20%, sehingga total harga yang harus anda bayar adalah {harga_akhir}.')
-else:
-    print('Anda tidak mendapat diskon')
